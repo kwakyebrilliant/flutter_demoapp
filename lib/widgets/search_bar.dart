871f0fc99199b9545210fe37b1edca7155aec443
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-29 12:20:45 +0000

    search updates

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -21,13 +21,12 @@ class _SearchBarState extends State<SearchBar> {
         height: 50.0,
         width: MediaQuery.of(context).size.width - 30.0,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: ColorPalette().searchBarFill,
-        ),
+            borderRadius: BorderRadius.circular(10.0),
+            color: ColorPalette().searchBarFill),
         child: TextField(
           decoration: InputDecoration(
               filled: true,
-              hintText: 'Find your coffee',
+              hintText: 'Find your coffee ...',
               contentPadding: const EdgeInsets.fromLTRB(10.0, 2.0, 5.0, 12.0),
               hintStyle:
                   GoogleFonts.sourceSansPro(color: const Color(0xFF525559)),
