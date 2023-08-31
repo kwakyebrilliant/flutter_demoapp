fe545456201ac72712f7d6fec20b4cded1d32ace
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-31 22:57:10 +0000

    details added

---
+++ lib/screens/item_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ItemDetails extends StatefulWidget {
+  const ItemDetails({super.key});
+
+  @override
+  State<ItemDetails> createState() => _ItemDetailsState();
+}
+
+class _ItemDetailsState extends State<ItemDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
