cb23f35e875594fb0dae7eba982dbba975749891
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-09-01 00:50:17 +0000

    details update

--- lib/screens/item_details.dart
+++ lib/screens/item_details.dart
@@ -67,6 +67,25 @@ class _ItemDetailsState extends State<ItemDetails> {
                       ),
                     ),
                   ),
+                  GestureDetector(
+                    onTap: () {
+                      // Navigator.of(context).pop();
+                    },
+                    child: Container(
+                      height: 45.0,
+                      width: 45.0,
+                      decoration: BoxDecoration(
+                          color: const Color(0xFF14181D),
+                          border: Border.all(
+                              color: const Color(0xFF322B2E), width: 1.0),
+                          borderRadius: BorderRadius.circular(15.0)),
+                      child: const Icon(
+                        Icons.favorite_rounded,
+                        color: Color(0xFF525154),
+                        size: 17.0,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
